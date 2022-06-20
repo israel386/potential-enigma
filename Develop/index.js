21037d6dda193df9ab76c9e29524d7600f3e2d20
israel386 <israel_delarosa@knights.ucf.edu>
2022-06-19 21:37:18 -0400

    Updated function renderLicenseBadge

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,54 @@ const inquirer = require('inquirer');
 const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Provide name of your project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide form of installation?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provie instuction and exampales for use'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Provide lisence',
+        choices: ['apache-2.0', 'agpl-3.0', 'gpl-3.0', 'lgpl-3.0', 'mpl-2.0', 'mit', 'unlicense']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'List contributers'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'provide examples on how to run project'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter gitHub username'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "apache-2.0") {
+    return `
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    `;
+  } if (license === "agpl-3.0") {
+    return `
+  <img src="https://img.shields.io/badge/License-AGPL%20v3-blue.svg" alt="agpl-3.0-badge">
+    `;
+  } if (license === "gpl-3.0") {
+    return `
+  <img src="https://img.shields.io/badge/License-GPL%20v3-blue.svg" alt="gpl-3.0-badge">
+    `;
+  } if (license === "lgpl-3.0") {
+    return `
+  <img src="https://img.shields.io/badge/License-LGPL%20v3-blue.svg" alt="lgpl-3.0-badge">
+    `;
+  } if (license === "mpl-2.0") {
+    return `
+  <img src="https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg" alt="MPL-2.0-badge">
+    `;
+  } if (license === "mit") {
+    return `
+  <img src="https://img.shields.io/badge/License-MIT-yellow.svg" alt="mit-badge">
+    `;
+  } if (license === "unlicense") {
+    return `
+  <img src="https://img.shields.io/badge/License-Unlicense-blue.svg" alt="unlicense-badge">
+    `;
+  }
+};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
